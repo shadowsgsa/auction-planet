ec9fb61eb697107ce2e1f59a819b096fee2fc542
SAUS INTERNATIONAL INC <darth3157@gmail.com>
2026-01-28 23:33:30 +0500

    Update API_URL to production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://www.theauctionplanet.com/api';
 // const API_URL = 'http://localhost:3001/api';
 
 const api = axios.create({
